812288156e577de715fc4d7b8ba55913f4adbb65
yakuhito <y@kuhi.to>
2023-04-21 21:03:45 -0700

    env variables & api

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function MyNavbar() {
   return (
     <Navbar bg="white" variant="pills" className="border-bottom mb-2 justify-content-center">
         <Navbar.Brand className="ml-4 text-center">
-            <Link href="https://v1-testnet10.tibetswap.io" passHref>
+            <Link href={`${process.env.NEXT_PUBLIC_APP_URL}`} passHref>
                 <Image
                     src="/logo.jpg"
                     height="64"
