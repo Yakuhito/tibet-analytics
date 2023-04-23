ef13cf4b6c867741f484ab621d4bdea4c0ba7c05
yakuhito <y@kuhi.to>
2023-04-22 18:20:33 -0700

    correctly display tokens (divide by 1000, not 10000) & set XCH display to 3 digits

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,4 @@
-export function formatWithComma(value: number, num_digits: number = 2): string {
+export function formatWithComma(value: number, num_digits: number = 3): string {
     return value.toLocaleString("en-US", {
         minimumFractionDigits: num_digits,
         maximumFractionDigits: num_digits,
@@ -12,7 +12,7 @@ export function formatToken(mojos: number, asChange: boolean = false): string {
         prefix = "- "
     }
 
-    return prefix + formatWithComma(mojos / 10 ** 4, 4);
+    return prefix + formatWithComma(mojos / 10 ** 3, 3);
 }
 
 export function formatDollars(value: number): string {
@@ -26,5 +26,5 @@ export function mojoToXCHString(value: number, showAsChange: boolean = false): s
         prefix = "- "
     }
 
-    return prefix + (value / (10 ** 12)).toFixed(2) + " " + process.env.NEXT_PUBLIC_XCH;
+    return prefix + (value / (10 ** 12)).toFixed(3) + " " + process.env.NEXT_PUBLIC_XCH;
 }
