f8021e8eb7b7d0ee823f9a8e2177198e24a217c8
Jonathan <jonathanlangton3@gmail.com>
2023-05-01 20:15:24 +0100

    Converted to tailwind & stopped it from fetching data until router has returned pair_id

--- src/pages/pair/[pair_id].tsx
+++ src/pages/pair/[pair_id].tsx
@@ -5,10 +5,12 @@ import { TransactionList } from '@/components/TransactionList';
 import { formatToken, mojoToXCHString } from '@/utils';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function PairDetails() {
   const router = useRouter();
-  var { pair_id } = router.query;
+  const { pair_id } = router.query;
 
   const PAGINATION = 42;
 
@@ -31,7 +33,10 @@ export default function PairDetails() {
       }
     }
 
-    fetchData();
+    // Only fetch pair data when id is fetched from router
+    if(pair_id) {
+      fetchData();
+    }
   }, [pair_id]);
 
   const loadMoreTxes = async () => {
@@ -54,22 +59,57 @@ export default function PairDetails() {
   }
 
   return (
-    <p>pair id</p>
-    // <div>
-    //   <Row>
-    //     <CustomCard title={`${process.env.NEXT_PUBLIC_XCH} Reserve`} value={mojoToXCHString(pair.xch_reserve)} subtitle="managed by pair" />
-    //     <CustomCard title={`${pair.short_name} Reserve`} value={`${formatToken(pair.token_reserve)} ${pair.short_name}`} subtitle="managed by pair" />
-    //     <CustomCard title="Liquidity" value={`${formatToken(pair.liquidity)}`} subtitle="liquidity tokens across all holders" />
-    //   </Row>
-    //   {transactions && (
-    //     <TransactionList
-    //       transactions={transactions}
-    //       tokenShortName={pair.short_name}
-    //       moarTxesAvailable={moarTxesAvailable}
-    //       loadingMoarTxes={loadingMoarTxes}
-    //       loadMoreTxes={loadMoreTxes}
-    //     />
-    //   )}
-    // </div>
+    <main>
+      {/* Token info Section */}
+      <section>
+
+        {/* Breadcrumb */}
+        <div className="flex items-center gap-4 select-none">
+          <Link href="/" className="text-xl font-medium text-brandDark/90 hover:opacity-60">Home</Link>
+          <p className="text-xl font-medium text-brandDark">›</p>
+          <p className="text-xl font-medium text-brandDark">SBX</p>
+        </div>
+
+        {/* Token Name */}
+        <div className="flex flex-col gap-4 py-12 pt-16">
+          <Image
+              src={pair.image_url}
+              alt="Token Image"
+              height={64}
+              width={64}
+              className="rounded-full"
+            />
+          <h1 className="font-bold text-5xl">
+            {pair.name}
+            <span className="text-brandDark/50 font-bold text-xl ml-2 sm:ml-4">({pair.short_name})</span>
+          </h1>
+        </div>
+
+        {/* Stats */}
+        <div className="w-full px-4 py-8 md:py-12 rounded-xl flex flex-col lg:flex-row md:justify-evenly gap-8 bg-brandDark bg-gradient-to-br from-[#7fa9b8] to-brandDark">
+          <CustomCard title={`${process.env.NEXT_PUBLIC_XCH} Reserve`} value={mojoToXCHString(pair.xch_reserve)} subtitle="managed by pair" />
+          <CustomCard title={`${pair.short_name} Reserve`} value={`${formatToken(pair.token_reserve)} ${pair.short_name}`} subtitle="managed by pair" />
+          <CustomCard title="Liquidity" value={`${formatToken(pair.liquidity)}`} subtitle="liquidity tokens across all holders" />
+        </div>
+      </section>
+
+
+      {/* Latest Transactions Section */}
+      <section>
+        <h2 className="font-bold text-5xl py-8 pt-16 pb-12">Transactions</h2>
+        <div>
+          {transactions && (
+            <TransactionList
+              transactions={transactions}
+              tokenShortName={pair.short_name}
+              moarTxesAvailable={moarTxesAvailable}
+              loadingMoarTxes={loadingMoarTxes}
+              loadMoreTxes={loadMoreTxes}
+            />
+          )}
+        </div>
+      </section>
+
+    </main>
   );
 }
