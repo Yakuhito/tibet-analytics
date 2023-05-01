e3b8e269d1791dce1517261cd814e134fc7890cf
Jonathan <jonathanlangton3@gmail.com>
2023-05-01 03:52:26 +0100

    Converted to tailwind with updated UI

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -1,26 +1,20 @@
 // Import React and necessary components
-import React from 'react';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import React, { ReactHTMLElement, ReactNode, ReactSVG } from 'react';
+import { DollarSign } from 'react-feather';
 
 // Define an interface for the component props
 interface CustomCardProps {
   title: string;
   value: string;
-  subtitle: string;
+  subtitle?: string;
 }
 
 // Create a functional component with TypeScript
 export const CustomCard: React.FC<CustomCardProps> = ({ title, value, subtitle }) => {
   return (
-    <Col md={4} lg={4} className="mb-4 mb-lg-4">
-      <Card>
-        <Card.Body className="text-center">
-          <Card.Text className="text-uppercase fs-5 fw-light">{title}</Card.Text>
-          <Card.Text className="text-dark mb-1 d-block h1">{value}</Card.Text>
-          <Card.Text className="text-lowercase fs-6 fw-light">{subtitle}</Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
+    <div>
+      <p className="text-brandLight/90 bg-brandLight/10 px-4 py-1 mb-4 rounded-full inline-flex items-center font-medium md:text-xl">{title}</p>
+      <p className="text-3xl xl:text-4xl font-bold text-white font-mono px-4 flex flex-col">{value} XCH <span className="text-sm font-normal text-brandLight/90 whitespace-nowrap">{subtitle}</span></p>
+  </div>
   );
 };
