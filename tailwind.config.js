61bd44d0632d4fed737bea41582003476cbdd88d
Jonathan <jonathanlangton3@gmail.com>
2023-05-01 03:53:57 +0100

    Added brand colours to tailwind colour library

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandDark: '#526e78',
+        brandLight: '#EFF4F7',
+      },
+    },
   },
   plugins: [],
 }
