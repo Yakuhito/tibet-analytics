8ce1195d3a54b99588f826276392cbdc7ea0ec01
Jonathan <jonathanlangton3@gmail.com>
2023-05-01 04:14:53 +0100

    Removed XCH suffix for transaction card

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -13,7 +13,7 @@ export const CustomCard: React.FC<CustomCardProps> = ({ title, value, subtitle }
   return (
     <div>
       <p className="text-brandLight/90 bg-brandLight/10 px-4 py-1 mb-4 rounded-full inline-flex items-center font-medium md:text-xl">{title}</p>
-      <p className="text-3xl xl:text-4xl font-bold text-white font-mono px-4 flex flex-col">{value} XCH <span className="text-sm font-normal text-brandLight/90 whitespace-nowrap">{subtitle}</span></p>
+      <p className="text-3xl xl:text-4xl font-bold text-white font-mono px-4 flex flex-col">{value}<span className="text-sm font-normal text-brandLight/90 whitespace-nowrap">{subtitle}</span></p>
   </div>
   );
 };
