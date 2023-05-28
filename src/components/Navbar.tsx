87693fcb1f48327308613abfcfba3bcb5357141d
Jonathan <jonathanlangton3@gmail.com>
2023-05-28 08:04:32 +0100

    Added dark mode style for selected nav item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
         <div className="flex items-center rounded-xl p-1">
           <Link href={`${process.env.NEXT_PUBLIC_APP_URL}`} className="hover:opacity-80 font-medium text-brandDark/70 dark:text-brandLight/50 px-6 py-1.5 rounded-xl">Swap</Link>
           <Link href={`${process.env.NEXT_PUBLIC_APP_URL}/faq`} className="hover:opacity-80 font-medium text-brandDark/70 dark:text-brandLight/50 px-6 py-1.5 rounded-xl">FAQs</Link>
-          <Link href="/" className="font-medium text-brandDark px-6 py-1.5 bg-brandDark/10 rounded-xl">Stats</Link>
+          <Link href="/" className="font-medium text-brandDark dark:text-brandLight px-6 py-1.5 bg-brandDark/10 rounded-xl">Stats</Link>
         </div>
       </div>
     </nav>
