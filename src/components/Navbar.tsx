e36a52f572eb94f53424687bef2e72618f2a66fb
Jonathan <jonathanlangton3@gmail.com>
2023-05-29 01:30:13 +0100

    Fix responsive UI bug on small devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav className="sticky w-full top-0 bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl">
-      <div className="container mx-auto px-4 flex gap-8 items-center justify-between py-4">
+    <header className="sticky w-full top-0 bg-brandLight/50 dark:bg-zinc-900/50 backdrop-blur-xl h-24">
+      <div className="container mx-auto px-4 flex gap-4 sm:gap-8 items-center justify-between py-4">
         <Link href="/">
             <Image
               src="/logo.jpg"
@@ -15,12 +15,12 @@ export default function Navbar() {
               className="rounded-full border-neutral-300 hover:translate-y-1 hover:opacity-80 transition dark:opacity-80"
             />
         </Link>
-        <div className="flex items-center rounded-xl p-1">
+        <nav className="flex items-center rounded-xl p-1">
           <Link href={`${process.env.NEXT_PUBLIC_APP_URL}`} className="hover:opacity-80 font-medium text-brandDark/70 dark:text-brandLight/50 px-6 py-1.5 rounded-xl">Swap</Link>
           <Link href={`${process.env.NEXT_PUBLIC_APP_URL}/faq`} className="hover:opacity-80 font-medium text-brandDark/70 dark:text-brandLight/50 px-6 py-1.5 rounded-xl">FAQs</Link>
           <Link href="/" className="font-medium text-brandDark dark:text-brandLight px-6 py-1.5 bg-brandDark/10 rounded-xl">Stats</Link>
-        </div>
+        </nav>
       </div>
-    </nav>
+    </header>
   );
 };
