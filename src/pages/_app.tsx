ea629b4b4e2b8a2ca22ae9ca8d90a9fae091bf4c
Jonathan <jonathanlangton3@gmail.com>
2023-07-08 19:10:43 +0100

    Added navbar component & code required app-wide for theme switcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,45 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react';
+import Navbar from '@/components/Navbar';
+import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  // Theme detector
+  const [theme, setTheme] = useState<"dark" | "light" | "auto">("auto");
+  useEffect(() => {
+    const detectTheme = () => {
+      if (typeof window !== 'undefined') {
+        if (localStorage.theme === 'dark') {
+          document.documentElement.classList.add('dark');
+          setTheme('dark');
+        } else if (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          document.documentElement.classList.add('dark');
+          setTheme('auto')
+        } else if (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches) {
+          document.documentElement.classList.remove('dark');
+          setTheme('auto')
+        } else {
+          document.documentElement.classList.remove('dark');
+          setTheme('light');
+        }
+      }
+    }
+    detectTheme()
+
+    // Detect user preference change
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', detectTheme);
+
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', detectTheme);
+    };
+
+  }, [theme]);
+
+  return (
+    <>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <Component {...pageProps} />
+    </>
+  )
 }
